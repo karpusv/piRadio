914e4ce3c0abb783ebb09d5dabdd10441fe182cb
Vadym Karpus <vadym.karpus@gmail.com>
2020-06-02 01:28:49 +0300

    Add stereo support

--- PiRadio.py
+++ PiRadio.py
@@ -3,6 +3,9 @@
 from subprocess import run
 
 
-def play_sound(filename, freq="100.0", sample_rate="22050"):
-    run(["./pifm", filename, freq, sample_rate])
+def play_sound(filename, freq="100.0", sample_rate="22050", stereo=True):
+    if(stereo):
+        run(["./pifm", filename, freq, sample_rate, "stereo"])
+    else:
+        run(["./pifm", filename, freq, sample_rate])
     return
