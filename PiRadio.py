cce869817aa7cb1c4529dab293919e066870e254
Vadym Karpus <vadym.karpus@gmail.com>
2020-06-02 21:11:24 +0300

    Capture piFM's output

--- PiRadio.py
+++ PiRadio.py
@@ -5,7 +5,7 @@ from subprocess import run
 
 def play_wav(filename, freq="100.0", sample_rate="22050", stereo=True):
     if(stereo):
-        run(["./pifm", filename, freq, sample_rate, "stereo"])
+        run(["./pifm", filename, freq, sample_rate, "stereo"],capture_output=True)
     else:
-        run(["./pifm", filename, freq, sample_rate])
+        run(["./pifm", filename, freq, sample_rate], capture_output=True)
     return
