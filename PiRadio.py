5637e60a36003ee8f5f77616c9a7161b084f7354
Vadym Karpus <vadym.karpus@icloud.com>
2020-06-04 01:25:20 +0300

    Remove hardcoded frequency

--- PiRadio.py
+++ PiRadio.py
@@ -15,12 +15,12 @@ def play_mp3(filename, freq="100.0", sample_rate="22050", stereo=False):
     if(stereo):
         ps = Popen(("ffmpeg", "-hide_banner", "-loglevel", "warning", "-i", filename, "-f",
                     "s16le", "-ar", sample_rate, "-ac", "2", "-af", "arealtime", "-"), stdout=PIPE)
-        run(["./pifm", "-", "91.1", sample_rate, "stereo"],
+        run(["./pifm", "-", freq, sample_rate, "stereo"],
             stdin=ps.stdout, capture_output=True)
 
     else:
         ps = Popen(("ffmpeg", "-hide_banner", "-loglevel", "warning", "-i", filename, "-f",
                     "s16le", "-ar", sample_rate, "-ac", "1", "-af", "arealtime", "-"), stdout=PIPE)
-        run(["./pifm", "-", "91.1", sample_rate],
+        run(["./pifm", "-", freq, sample_rate],
             stdin=ps.stdout, capture_output=True)
     return
